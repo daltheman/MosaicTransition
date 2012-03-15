f354894be797e58a7f2f4cfdba4614488dc0a3a4
Danilo Altheman <danilo.a@quaddro.com.br>
2012-03-15 02:33:50 -0300

    Initial Commit

---
+++ MosaicTransition/NSArray+Shuffle.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Shuffle.h
+//  MosaicTransition
+//
+//  Created by Danilo Altheman on 3/15/12.
+//  Copyright (c) 2012 Quaddro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Shuffle)
+
+@end
---
+++ MosaicTransition/NSArray+Shuffle.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+Shuffle.m
+//  MosaicTransition
+//
+//  Created by Danilo Altheman on 3/15/12.
+//  Copyright (c) 2012 Quaddro. All rights reserved.
+//
+
+#import "NSArray+Shuffle.h"
+
+@implementation NSArray (Shuffle)
+
+@end
